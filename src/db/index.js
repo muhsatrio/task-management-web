4c11dc49a3d1913bd30ef3d26e90a214686dcf8d
muhsatrio <muhsatrio@gmail.com>
2020-08-28 14:01:23 +0700

    fix(axios): fix codebase of axios

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,19 @@
-const instance = require('./instance');
+const axios = require('axios');
+
+const instance = axios.create({
+    baseURL: 'http://admin:iniadmin@13.250.43.79:5984/'
+});
 
 const db = {
     getAllDocuments: async (dbName) => {
         const result = await instance.get(`/${dbName}/_all_docs`);
         return result;
+    },
+    getDocumentsByID: async (dbName, id) => {
+        const result = await instance.get(`/${dbName}/${id}`);
+        return result;
     }
+
 }
 
 export default db;
--- src/db/instance.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const instance = axios.create({
-    baseURL: 'http://admin:iniadmin@13.250.43.79:5984/'
-});
-
-export default instance;
