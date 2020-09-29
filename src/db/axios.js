f242b5251eec88190daa7f16d3adddd2d6cda0fa
muhsatrio <muhsatrio@gmail.com>
2020-09-29 09:58:35 +0700

    feat: redevelop environment variable

--- src/db/axios.js
+++ src/db/axios.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+require('dotenv').config();
 
 const serverAPI = axios.create({
     baseURL: `http://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/`
